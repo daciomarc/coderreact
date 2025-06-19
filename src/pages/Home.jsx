42626b48baa12dfcd31266d1c7891b51789c34a5
daciomarc <dacio.marcon@gmail.com>
2025-06-19 20:24:23 -0300

    corrigiendo el home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,16 +3,22 @@ import { getAllProducts } from "../services/products.service";
 import ItemListContainer from "../components/ItemListContainer";
 
 const Home = () => {
-
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    getAllProducts().then((res) => {
-      setProducts(res.data.products);
-    });
+    getAllProducts()
+      .then((res) => {
+        setProducts(res.data.products);
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   }, []);
-
-  return <ItemListContainer products={products}/>;
+  return loading ? <>Cargando...</> : <ItemListContainer products={products} />;
 };
 
 export default Home;
