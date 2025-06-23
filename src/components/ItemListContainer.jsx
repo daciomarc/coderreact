b67a4a3db523a9170a21b1a30581c513de09f901
daciomarc <dacio.marcon@gmail.com>
2025-06-23 18:43:32 -0300

    ajustando categories

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -7,7 +7,7 @@ const ItemCard = ({ id, image, title, description, price, discount }) => {
 
   return (
     <Box
-      bgGradient={"linear(to-r, red.400, yellow.400)"}
+      bgGradient={"linear(to-r, blue.400, purple.400)"}
       width={"300px"}
       borderWidth={"1px"}
       borderRadius={"lg"}
